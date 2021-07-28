bab8105d9b068d410fbc9a636c6d876ba2b3d248
Mpartx21 <miguel.p@revature.net>
2021-07-28 10:04:16 -0700

    Demo 14 and 15

--- Week-1/Java/src/com/miguel/day3/Demo14.java
+++ Week-1/Java/src/com/miguel/day3/Demo14.java
@@ -1,4 +1,9 @@
 package com.miguel.day3;
 
 public class Demo14 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append("World");
+        System.out.println(sb);
+    }
 }
